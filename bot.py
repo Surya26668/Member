04daf69c7e3bf29026fcc75bcc931f711955be40
Surya26668 <158288653+Surya26668@users.noreply.github.com>
2024-06-13 02:09:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ owner = 6766253056
 whitelist = {}
 datetime_format = f"%H:%M:%S 6948368900:AAElseNx9zbGKCe3es4YUpFQ_37G0y1UL2Ed-%m-%Y"
 wib = timezone('Asia/Jakarta')
-bot_token = '7369068314:AAENMxNQMDwqcEs5vyTA-AFWrU_TMphKEbk'
+bot_token = '6948368900:AAElseNx9zbGKCe3es4YUpFQ_37G0y1UL2E'
 bot = AsyncTeleBot(bot_token, parse_mode='HTML', state_storage=StateMemoryStorage())
 
 bot.add_custom_filter(StateFilter(bot))
