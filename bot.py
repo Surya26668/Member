e6e0cdaf4a4b9a9aca9bf6332ab50c4fc12ee23f
Surya26668 <158288653+Surya26668@users.noreply.github.com>
2024-06-13 03:12:16 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telebot.asyncio_storage import StateMemoryStorage
 
 owner = [6766253056]
 whitelist = {}
-datetime_format = f"%H:%M:%S 6948368900:AAElseNx9zbGKCe3es4YUpFQ_37G0y1UL2Ed-%m-%Y"
+datetime_format = f"%H:%M:%S d-%m-%Y"
 wib = timezone('Asia/Jakarta')
 bot_token = '6948368900:AAElseNx9zbGKCe3es4YUpFQ_37G0y1UL2E'
 bot = AsyncTeleBot(bot_token, parse_mode='HTML', state_storage=StateMemoryStorage())
